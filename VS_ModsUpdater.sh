7485efbee23087331e72ff2c3a42ebee22dafb9d
Laerinok <laerinok@gmail.com>
2023-10-19 17:02:12 +0200

    v1.2.0 - script .sh

---
+++ VS_ModsUpdater.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Script to run VS_ModsUpdater with server.
+# You must adapt it to your configuration and 
+# First set the path to VS_ModsUpdater
+# Then set the mods directory
+# Change the language if needed (same name than the lang file without the extension)
+# nopause must be set to true for server use.
+'/Path_To_VS_ModsUpdater/VS_ModsUpdater' --modspath 'Path_To_Your_Mods_Directory' --language en_US --nopause true
